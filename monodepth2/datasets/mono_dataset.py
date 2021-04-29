0245b3dd21a367b4c9426d1816c05db1c15319b9
Inbar Fried <inbarfried@Inbars-MacBook-Pro.local>
2021-04-29 11:29:58 -0700

    forgot to include i value in mono_dataset code

--- monodepth2/datasets/mono_dataset.py
+++ monodepth2/datasets/mono_dataset.py
@@ -166,8 +166,10 @@ class MonoDataset(data.Dataset):
             # stereo
             # uses left/right image pairs
             # gets appended to frame_idxs in trainer.py
+            i = 0
             inputs[("color", i, -1)] = self.get_color(folder, frame_index, side, do_flip)
             # read other correspondong image
+            i = "s"
             other_side = {"r": "l", "l": "r"}[side]
             inputs[("color", i, -1)] = self.get_color(folder, frame_index, other_side, do_flip)
         # original code
